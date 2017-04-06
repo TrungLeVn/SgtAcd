3ef9f1c9165b9aef3af3f0b940cac51effc38f42
Trung Le <trunglehai.hvnh@gmail.com>
2017-04-06 12:04:56 +0100

    Revise the startpars function to allow for arch, skm, kum in the mean equation by two steps: 1: Fit the model with no arch, skm kum. Collect the conditional vol, skewness and kurtosis. 2. Fit the data to the condional vol, skewness, kurtosis using simple OLS. Using the fitted coef for the starting parameters

--- R/acd-startpars.R
+++ R/acd-startpars.R
@@ -8,7 +8,7 @@ TinY = 1e-08
 # Starting parameters for mean equation using ARMA fit
 #-------
 .meqstartpars = function(pars, arglist) {
-  data = arglist$data
+  data = zoo::zoo(arglist$data,order.by = arglist$index);
   N = length(as.numeric(unlist(data)))
   model = arglist$model
   modelinc = model$modelinc
@@ -16,7 +16,40 @@ TinY = 1e-08
   idx = model$pidx
   tmph = 0
   # Get the estimation for ARMA specfication in mean equation.  To be used to find the starting parameters in variance equation
-   if (modelinc[2] > 0 | modelinc[3] > 0) {
+  if (sum(modelinc[4:6]) > 0){
+    tempspec = acdspec(mean.model = list(armaOrder = FALSE, skm = FALSE, shm = FALSE),
+                       distribution.model = model$dmodel, variance.model = model$vmodel)
+    tempfit = acdfit(tempspec,data = data)
+    if(tempfit@fit$convergence!=0){
+      tempfit = acdfit(tempspec, data = data,solver = "mssolnp")
+      if(tempfit@fit$convergence!=0){
+        stop("\nacdfit-->error: could not find appropriate starting values for recursion\n")
+      }
+    }
+    tmph = cbind(archm = tempfit$sigma,skm = tempfit$skewness,kum = tempfit$kurtosis)
+    mexdata = NULL
+    if(modelinc[4] > 0){
+      mexdata = cbind(mexdata,tmph[,"archm"])
+    }
+    if(modelin[5]>0){
+      mexdata = cbind(mexdata,tmph[,"skm"])
+    }
+    if(modelinc[6]>0){
+      mexdata = cbind(mexdata,tmph[,"kum"])
+    }
+    y = coredata(data);
+    fit.mean = lm(y ~ mexdata)
+    pars[idx["mu", 1]:idx["mu", 2], 1] = fit.mean$coef["(Intercept)"]
+    if(modelinc[4]>0){
+      pars[idx["archm", 1]:idx["archm", 2], 1] = fit.mean$coef["archm"]
+    }
+    if(modelinc[5]>0){
+      pars[idx["skm", 1]:idx["skm", 2], 1] = fit.mean$coef["skm"]
+    }
+    if(modelinc[6]>0){
+      pars[idx["kum", 1]:idx["kum", 2], 1] = fit.mean$coef["kum"]
+    }
+  } else if (modelinc[2] > 0 | modelinc[3] > 0) {
     ttemp = arima(data, order = c(modelinc[2], 0, modelinc[3]), include.mean = modelinc[1], method = "CSS")
     fit.mean = ttemp$coef
     if (modelinc[1] > 0) {
@@ -95,6 +128,60 @@ TinY = 1e-08
       pars[manames[mamatch], 6] = as.numeric(fixed.pars[j])
     }
   }
+  # archm
+  if (modelinc[4] > 0) {
+    manames = paste("archm", 1:modelinc[4], sep = "")
+    pars[idx["archm", 1]:idx["archm", 2], 5] = -1 + TinY
+    pars[idx["archm", 1]:idx["archm", 2], 6] = 1 - TinY
+    if (any(substr(start.names, 1, 2) == "archm")) {
+      j = which(substr(start.names, 1, 2) == "archm")
+      mamatch = charmatch(start.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(start.pars[j])
+    }
+    if (any(substr(fixed.names, 1, 2) == "archm")) {
+      j = which(substr(fixed.names, 1, 2) == "archm")
+      mamatch = charmatch(fixed.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 5] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 6] = as.numeric(fixed.pars[j])
+    }
+  }
+  # skm
+  if (modelinc[5] > 0) {
+    manames = paste("skm", 1:modelinc[5], sep = "")
+    pars[idx["skm", 1]:idx["skm", 2], 5] = -5 + TinY
+    pars[idx["skm", 1]:idx["skm", 2], 6] = 5 - TinY
+    if (any(substr(start.names, 1, 2) == "skm")) {
+      j = which(substr(start.names, 1, 2) == "skm")
+      mamatch = charmatch(start.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(start.pars[j])
+    }
+    if (any(substr(fixed.names, 1, 2) == "skm")) {
+      j = which(substr(fixed.names, 1, 2) == "skm")
+      mamatch = charmatch(fixed.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 5] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 6] = as.numeric(fixed.pars[j])
+    }
+  }
+  # kum
+  if (modelinc[6] > 0) {
+    manames = paste("kum", 1:modelinc[6], sep = "")
+    pars[idx["kum", 1]:idx["kum", 2], 5] = -5 + TinY
+    pars[idx["kum", 1]:idx["kum", 2], 6] = 5 - TinY
+    if (any(substr(start.names, 1, 2) == "kum")) {
+      j = which(substr(start.names, 1, 2) == "kum")
+      mamatch = charmatch(start.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(start.pars[j])
+    }
+    if (any(substr(fixed.names, 1, 2) == "kum")) {
+      j = which(substr(fixed.names, 1, 2) == "kum")
+      mamatch = charmatch(fixed.names[j], manames)
+      pars[manames[mamatch], 1] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 5] = as.numeric(fixed.pars[j])
+      pars[manames[mamatch], 6] = as.numeric(fixed.pars[j])
+    }
+  }
   return(pars)
 }
 #-----
@@ -121,10 +208,10 @@ acdstart = function(pars, arglist) {
   assign("garchLL", NA, envir = garchenv)
   dbounds = arglist$sbounds
   bounds = .DistributionBounds(model$dmodel$model)
-  numdispar = sum(modelinc[c(11,15,19)])
+  numdispar = sum(modelinc[c(14,19,24)])
   arglist$skhEst = rep(0,1,numdispar)
   # to be used for starting the recursion in case of problems
-  if (modelinc[4] > 0) {
+  if (modelinc[7] > 0) {
     pars[idx["omega", 1]:idx["omega", 2], 5] = var(data)/1e+05
     pars[idx["omega", 1]:idx["omega", 2], 6] = var(data) * 1e+05
     if (is.null(start.pars$omega))
@@ -135,7 +222,7 @@ acdstart = function(pars, arglist) {
       pars[idx["omega", 1]:idx["omega", 2], 6] = fixed.pars$omega
     }
   }
-  if (modelinc[5] > 0) {
+  if (modelinc[8] > 0) {
     pars[idx["alpha", 1]:idx["alpha", 2], 5] = 0 + TinY
     pars[idx["alpha", 1]:idx["alpha", 2], 6] = 0.6 -TinY
     if (is.null(start.pars$alpha))
@@ -146,7 +233,7 @@ acdstart = function(pars, arglist) {
       pars[idx["alpha", 1]:idx["alpha", 2], 6] = fixed.pars$alpha
     }
   }
-  if (modelinc[6] > 0) {
+  if (modelinc[9] > 0) {
     pars[idx["beta", 1]:idx["beta", 2], 5] = 0.2 + TinY
     pars[idx["beta", 1]:idx["beta", 2], 6] = 1-TinY
     if (is.null(start.pars$beta))
@@ -157,7 +244,7 @@ acdstart = function(pars, arglist) {
       pars[idx["beta", 1]:idx["beta", 2], 6] = fixed.pars$beta
     }
   }
-  if (modelinc[7] > 0) {
+  if (modelinc[10] > 0) {
     pars[idx["gamma", 1]:idx["gamma", 2], 5] = -.5+ TinY
     pars[idx["gamma", 1]:idx["gamma", 2], 6] = 1-TinY
     if (is.null(start.pars$gamma))
@@ -168,7 +255,7 @@ acdstart = function(pars, arglist) {
       pars[idx["gamma", 1]:idx["gamma", 2], 6] = fixed.pars$gamma
     }
   }
-  if (modelinc[8] > 0) {
+  if (modelinc[11] > 0) {
     if (is.na(pars[idx["skew", 1]:idx["skew", 2], 5]))
       pars[idx["skew", 1]:idx["skew", 2], 5] = dbounds[1]
     if (is.na(pars[idx["skew", 1]:idx["skew", 2], 6]))
@@ -181,7 +268,7 @@ acdstart = function(pars, arglist) {
         pars[idx["skew", 1]:idx["skew", 2], 6] = as.numeric(fixed.pars$skew)
       }
   }
-  if (modelinc[9] > 0) {
+  if (modelinc[12] > 0) {
     if (is.na(pars[idx["shape1", 1]:idx["shape1", 2], 5]))
       pars[idx["shape1", 1]:idx["shape1", 2], 5] = dbounds[3]
     if (is.na(pars[idx["shape1", 1]:idx["shape1", 2], 6]))
@@ -194,7 +281,7 @@ acdstart = function(pars, arglist) {
         pars[idx["shape1", 1]:idx["shape1", 2], 6] = NA
       }
   }
-  if (modelinc[10] > 0) {
+  if (modelinc[13] > 0) {
     if (is.na(pars[idx["shape2", 1]:idx["shape2", 2], 5]))
       pars[idx["shape2", 1]:idx["shape2", 2], 5] = dbounds[5]
     if (is.na(pars[idx["shape2", 1]:idx["shape2", 2], 6]))
@@ -207,9 +294,9 @@ acdstart = function(pars, arglist) {
         pars[idx["shape2", 1]:idx["shape2", 2], 6] = NA
       }
   }
-  if (modelinc[11] > 0) {
+  if (modelinc[14] > 0) {
     uncskew = bounds$skew
-    xskew = .acdskewbounds(modelinc[11:13], uncskew, model$dmodel$model, dbounds[1:2])
+    xskew = .acdskewbounds(modelinc[15:17], uncskew, model$dmodel$model, dbounds[1:2])
     arglist$skhEst[1] = xskew$sk0
     if (is.na(pars[idx["skcons", 1]:idx["skcons", 2], 5]))
       pars[idx["skcons", 1]:idx["skcons", 2], 5] = xskew$skewpar.LB[1]
@@ -222,7 +309,7 @@ acdstart = function(pars, arglist) {
       pars[idx["skcons", 1]:idx["skcons", 2], 5] = as.numeric(fixed.pars$skcons)
       pars[idx["skcons", 1]:idx["skcons", 2], 6] = as.numeric(fixed.pars$skcons)
     }
-    if (modelinc[12] > 0) {
+    if (modelinc[15] > 0) {
       if (is.na(pars[idx["skalpha", 1]:idx["skalpha", 2], 5]))
         pars[idx["skalpha", 1]:idx["skalpha", 2], 5] = xskew$skewpar.LB[2]
       if (is.na(pars[idx["skalpha", 1]:idx["skalpha", 2], 6]))
@@ -235,7 +322,7 @@ acdstart = function(pars, arglist) {
           pars[idx["skalpha", 1]:idx["skalpha", 2], 6] = as.numeric(fixed.pars$skalpha)
         }
     }
-    if (modelinc[13] > 0) {
+    if (modelinc[16] > 0) {
       if (is.na(pars[idx["skgamma", 1]:idx["skgamma", 2], 5]))
         pars[idx["skgamma", 1]:idx["skgamma", 2], 5] = xskew$skewpar.LB[3]
       if (is.na(pars[idx["skgamma", 1]:idx["skgamma", 2], 6]))
@@ -248,7 +335,7 @@ acdstart = function(pars, arglist) {
           pars[idx["skgamma", 1]:idx["skgamma", 2], 6] = as.numeric(fixed.pars$skgamma)
         }
     }
-    if (modelinc[14] > 0) {
+    if (modelinc[17] > 0) {
       if (is.na(pars[idx["skbeta", 1]:idx["skbeta", 2], 5]))
         pars[idx["skbeta", 1]:idx["skbeta", 2], 5] = xskew$skewpar.LB[4]
       if (is.na(pars[idx["skbeta", 1]:idx["skbeta", 2], 6]))
@@ -261,10 +348,23 @@ acdstart = function(pars, arglist) {
           pars[idx["skbeta", 1]:idx["skbeta", 2], 6] = as.numeric(fixed.pars$skbeta)
         }
     }
+    if (modelinc[18] > 0) {
+    if (is.na(pars[idx["volsk", 1]:idx["volsk", 2], 5]))
+      pars[idx["volsk", 1]:idx["volsk", 2], 5] = -2
+    if (is.na(pars[idx["volsk", 1]:idx["volsk", 2], 6]))
+      pars[idx["volsk", 1]:idx["volsk", 2], 6] = 2
+    if (is.null(start.pars$volsk))
+      pars[idx["volsk", 1]:idx["volsk", 2], 1] = 0 else pars[idx["volsk", 1]:idx["volsk", 2], 1] = start.pars$volsk[1]
+      if (any(substr(fixed.names, 1, 6) == "volsk")) {
+        pars[idx["volsk", 1]:idx["volsk", 2], 1] = as.numeric(fixed.pars$volsk)
+        pars[idx["volsk", 1]:idx["volsk", 2], 5] = as.numeric(fixed.pars$volsk)
+        pars[idx["volsk", 1]:idx["volsk", 2], 6] = as.numeric(fixed.pars$volsk)
+      }
   }
-  if (modelinc[15] > 0) {
+}
+  if (modelinc[19] > 0) {
     uncshape1 = bounds$shape1
-    xshape1 = .acdshape1bounds(modelinc[16:18], uncshape1, model$dmodel$model, dbounds[c(3,4,7)])
+    xshape1 = .acdshape1bounds(modelinc[20:22], uncshape1, model$dmodel$model, dbounds[c(3,4,7)])
     arglist$skhEst[2] = xshape1$sh0
     pxd = which(is.na(pars[idx["sh1cons", 1]:idx["sh1cons", 2], 5]))
     if (length(pxd) > 0)
@@ -280,7 +380,7 @@ acdstart = function(pars, arglist) {
       pars[idx["sh1cons", 1]:idx["sh1cons", 2], 6] = as.numeric(fixed.pars$sh1cons)
     }
 
-    if (modelinc[16] > 0) {
+    if (modelinc[20] > 0) {
       if (is.na(pars[idx["sh1alpha", 1]:idx["sh1alpha", 2], 5]))
         pars[idx["sh1alpha", 1]:idx["sh1alpha", 2], 5] = xshape1$shapepar.LB[2]
       if (is.na(pars[idx["sh1alpha", 1]:idx["sh1alpha", 2], 6]))
@@ -293,7 +393,7 @@ acdstart = function(pars, arglist) {
           pars[idx["sh1alpha", 1]:idx["sh1alpha", 2], 6] = as.numeric(fixed.pars$sh1alpha)
         }
     }
-    if (modelinc[17] > 0) {
+    if (modelinc[21] > 0) {
       if (is.na(pars[idx["sh1gamma", 1]:idx["sh1gamma", 2], 5]))
         pars[idx["sh1gamma", 1]:idx["sh1gamma", 2], 5] = xshape1$shapepar.LB[3]
       if (is.na(pars[idx["sh1gamma", 1]:idx["sh1gamma", 2], 6]))
@@ -306,7 +406,7 @@ acdstart = function(pars, arglist) {
           pars[idx["sh1gamma", 1]:idx["sh1gamma", 2], 6] = as.numeric(fixed.pars$sh1gamma)
         }
     }
-    if (modelinc[18] > 0) {
+    if (modelinc[22] > 0) {
       if (is.na(pars[idx["sh1beta", 1]:idx["sh1beta", 2], 5]))
         pars[idx["sh1beta", 1]:idx["sh1beta", 2], 5] = xshape1$shapepar.LB[4]
       if (is.na(pars[idx["sh1beta", 1]:idx["sh1beta", 2], 6]))
@@ -319,10 +419,23 @@ acdstart = function(pars, arglist) {
           pars[idx["sh1beta", 1]:idx["sh1beta", 2], 6] = as.numeric(fixed.pars$sh1beta)
         }
     }
-  }
-  if (modelinc[19] > 0) {
+    if (modelinc[23] > 0) {
+  if (is.na(pars[idx["volsh1", 1]:idx["volsh1", 2], 5]))
+    pars[idx["volsh1", 1]:idx["volsh1", 2], 5] = -2
+  if (is.na(pars[idx["volsh1", 1]:idx["volsh1", 2], 6]))
+    pars[idx["volsh1", 1]:idx["volsh1", 2], 6] = 2
+  if (is.null(start.pars$volsh1))
+    pars[idx["volsh1", 1]:idx["volsh1", 2], 1] = 0 else pars[idx["volsh1", 1]:idx["volsh1", 2], 1] = start.pars$volsh1[1]
+    if (any(substr(fixed.names, 1, 6) == "volsh1")) {
+      pars[idx["volsh1", 1]:idx["volsh1", 2], 1] = as.numeric(fixed.pars$volsh1)
+      pars[idx["volsh1", 1]:idx["volsh1", 2], 5] = as.numeric(fixed.pars$volsh1)
+      pars[idx["volsh1", 1]:idx["volsh1", 2], 6] = as.numeric(fixed.pars$volsh1)
+    }
+}
+}
+  if (modelinc[24] > 0) {
       uncshape2 = bounds$shape2
-      xshape2 = .acdshape2bounds(modelinc[20:22], uncshape2, model$dmodel$model, dbounds[5:7])
+      xshape2 = .acdshape2bounds(modelinc[25:27], uncshape2, model$dmodel$model, dbounds[5:7])
       arglist$skhEst[3] = xshape2$sh0
       pxd = which(is.na(pars[idx["sh2cons", 1]:idx["sh2cons", 2], 5]))
       if (length(pxd) > 0)
@@ -340,7 +453,7 @@ acdstart = function(pars, arglist) {
         pars[idx["sh2cons", 1]:idx["sh2cons", 2], 6] = as.numeric(fixed.pars$sh2cons)
       }
 
-      if (modelinc[20] > 0) {
+      if (modelinc[25] > 0) {
         if (is.na(pars[idx["sh2alpha", 1]:idx["sh2alpha", 2], 5]))
           pars[idx["sh2alpha", 1]:idx["sh2alpha", 2], 5] = xshape2$shapepar.LB[2]
         if (is.na(pars[idx["sh2alpha", 1]:idx["sh2alpha", 2], 6]))
@@ -353,7 +466,7 @@ acdstart = function(pars, arglist) {
             pars[idx["sh2alpha", 1]:idx["sh2alpha", 2], 6] = as.numeric(fixed.pars$sh2alpha)
           }
       }
-      if (modelinc[21] > 0) {
+      if (modelinc[26] > 0) {
         if (is.na(pars[idx["sh2gamma", 1]:idx["sh2gamma", 2], 5]))
           pars[idx["sh2gamma", 1]:idx["sh2gamma", 2], 5] = xshape2$shapepar.LB[3]
         if (is.na(pars[idx["sh2gamma", 1]:idx["sh2gamma", 2], 6]))
@@ -366,7 +479,7 @@ acdstart = function(pars, arglist) {
             pars[idx["sh2gamma", 1]:idx["sh2gamma", 2], 6] = as.numeric(fixed.pars$sh2gamma)
           }
       }
-      if (modelinc[22] > 0) {
+      if (modelinc[27] > 0) {
         if (is.na(pars[idx["sh2beta", 1]:idx["sh2beta", 2], 5]))
           pars[idx["sh2beta", 1]:idx["sh2beta", 2], 5] = xshape2$shapepar.LB[4]
         if (is.na(pars[idx["sh2beta", 1]:idx["sh2beta", 2], 6]))
@@ -377,6 +490,19 @@ acdstart = function(pars, arglist) {
             pars[idx["sh2beta", 1]:idx["sh2beta", 2], 1] = as.numeric(fixed.pars$sh2beta)
             pars[idx["sh2beta", 1]:idx["sh2beta", 2], 5] = as.numeric(fixed.pars$sh2beta)
             pars[idx["sh2beta", 1]:idx["sh2beta", 2], 6] = as.numeric(fixed.pars$sh2beta)
+          }
+      }
+      if (modelinc[28] > 0) {
+        if (is.na(pars[idx["volsh2", 1]:idx["volsh2", 2], 5]))
+          pars[idx["volsh2", 1]:idx["volsh2", 2], 5] = -2
+        if (is.na(pars[idx["volsh2", 1]:idx["volsh2", 2], 6]))
+          pars[idx["volsh2", 1]:idx["volsh2", 2], 6] = 2
+        if (is.null(start.pars$volsh2))
+          pars[idx["volsh2", 1]:idx["volsh2", 2], 1] = 0 else pars[idx["volsh2", 1]:idx["volsh2", 2], 1] = start.pars$volsh2[1]
+          if (any(substr(fixed.names, 1, 6) == "volsh2")) {
+            pars[idx["volsh2", 1]:idx["volsh2", 2], 1] = as.numeric(fixed.pars$volsh2)
+            pars[idx["volsh2", 1]:idx["volsh2", 2], 5] = as.numeric(fixed.pars$volsh2)
+            pars[idx["volsh2", 1]:idx["volsh2", 2], 6] = as.numeric(fixed.pars$volsh2)
           }
       }
   }
